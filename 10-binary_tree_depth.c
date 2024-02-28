8ba67e74be6192ecedf53c94a8defea449d302cd
Mohammed Refat <mohammedrefat53@outlook.com>
2024-02-28 13:15:09 +0200

    task 9-10

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_depth - measures the depth of a node in the tree
+ * @tree:  pointer to the node
+ * Return: depth of the binary tree, 0 if not found
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t depth  = 0;
+
+	while (tree)
+	{
+		tree = tree->parent;
+		depth++;
+	}
+	return depth;
+
+}
---
+++ 9-binary_tree_height.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree:  pointer to the root node of the tree
+ * Return: height of the binary tree, 0 if not found
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height = 0;
+	size_t right_height = 0;
+
+	if (!tree)
+	{
+		return (0);
+	}
+	if (tree->left)
+	{
+		left_height = 1 + binary_tree_height(tree->left);
+	}
+	if (tree->right)
+	{
+		right_height = 1 + binary_tree_height(tree->right);
+	}
+	return (left_height > right_height? left_height : right_height);
+}
