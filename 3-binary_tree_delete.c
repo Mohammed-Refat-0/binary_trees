1cdc28ea3b30e05d05fe672d35595c136d228974
Mohammed Refat <mohammedrefat53@outlook.com>
2024-02-28 10:29:28 +0200

    task 3

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - deletes an entire binary tree
+ * @tree: Pointer to root node of tree to delete
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (!tree)
+	{
+		return;
+	}
+
+	if (tree != NULL)
+	{
+		binary_tree_delete(tree->left);
+		binary_tree_delete(tree->right);
+		free(tree);
+	}
+	return;
+}
